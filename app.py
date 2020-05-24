412df01ab16dd6f7ef4ca9ab91582b9f7b1ce91f
Shogo Narishige <narishige36@gmail.com>
2020-05-25 00:27:20 +0900

    テーマが入力されていない時次のページに移動しないよう修正

--- app.py
+++ app.py
@@ -22,6 +22,9 @@ def top():
     else:
         #テーマ名を入力してDBに保存
         theme = request.form.get("theme")
+        if theme == "":
+            noTheme = "テーマが入力されていません"
+            return render_template("index.html",noTheme = noTheme)
         conn = sqlite3.connect('service.db')
         c = conn.cursor()
         c.execute("insert into themes values(null,?)", (theme,) )
@@ -30,9 +33,9 @@ def top():
         theme_id = c.fetchone()
         conn.commit()
         conn.close()
-        # user_id が NULL(PythonではNone)じゃなければログイン成功
+        # theme_id が NULL(PythonではNone)じゃなければログイン成功
         if theme_id is None:
-            # ログイン失敗すると、ログイン画面に戻す
+            # theme_id が設定されていないとトップページに戻す
             return render_template("index.html")
         else:
             session['theme_id'] = theme_id[0]
@@ -42,7 +45,11 @@ def top():
 @app.route('/shiritori',methods=["GET","POST"])
 def shiritori():
     if request.method == "GET":
-        return render_template("shiritori.html")
+        if theme_id is None:
+            # theme_id が設定されていないとトップページに戻す
+            return render_template("index.html")
+        else:
+            return render_template("shiritori.html")
     else:
         # １開始ボタンを押すとしりとりゲームが始まる
         # 2 60秒のカウントダウン
