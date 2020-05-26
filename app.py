63a80883511507b929338361154153e4ecd8a774
Mio Orita <sakuraminoru0414@gmail.com>
2020-05-26 17:54:25 +0900

    テーマとしりとりの取り出し、インサート文の場所変更

--- app.py
+++ app.py
@@ -90,32 +90,41 @@ def remind():
             conn = sqlite3.connect("service.db")
             c = conn.cursor()
             c.execute("select theme from themes where id = ?", (theme_id,))
-            theme = c.fetchone()
+            theme_tuple = c.fetchone() # タプル型('テーマ',)
             c.close()
+            print(theme_tuple[0]) # テーマ
 
             # しりとりの内容を取ってくる
             conn = sqlite3.connect("service.db")
             c = conn.cursor()
-            c.execute("select word,id from shiritori where theme_id = ?", (theme_id,))
+            c.execute("select id,word from shiritori where theme_id = ?", (theme_id,))
             shiritori_list = []
-            # shiritori_listに取ってきた内容を辞書型格納
+            # shiritori_listに取ってきた内容をリスト内辞書型格納
             for row in c.fetchall():
                 shiritori_list.append({'id': row[0], 'word': row[1]})
-            c.close()  # closeはfor文の外で！
-            # 中身を確認
-            print(shiritori_list)
-            shiritori_id = shiritori_list[0]
+            c.close()
+            print(shiritori_list) # [{'id':1, 'word':'ひ'}, {'id':2, 'word':'ふ'}, {'id':3, 'word':'み'}]
 
-            #db remind に 連想をインサート
-            conn = sqlite3.connect('service.db')
-            c = conn.cursor()
-            c.execute("insert into remind values(?,?,?,null)", (theme_id,shiritori_id,remind,) )
-            conn.commit()
-            conn.close()
+            # wordだけのリスト型
+            s_word = [w['word'] for w in shiritori_list] # ['ひ','ふ','み']
+            for k in s_word:
+                print(k) # ひ ふ み
 
-            return render_template("remind.html", shiritori_word = shiritori_list[1], theme = theme)
+            # in range (len(shiritori_list))
+
+            return render_template("remind.html", shiritori_word = k , theme = theme_tuple[0])
     else:
-        return redirect("favorite.html")
+        # しりとりidリスト型
+        shiritori_id = [d['id'] for d in shiritori_list] # [1,2,3]
+        for k in shiritori_id:
+            print(k) # 1 2 3
+        #db remind に 連想をインサート
+        conn = sqlite3.connect('service.db')
+        c = conn.cursor()
+        c.execute("insert into remind values(?,?,?,null)", (theme_id,shiritori_id,remind,))
+        conn.commit()
+        conn.close()
+        return render_template("favorite.html")
 
 
 
