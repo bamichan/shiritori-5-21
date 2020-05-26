1eaba643cb4c12ca11a65a740accf1dc26bdd2e4
Mio Orita <sakuraminoru0414@gmail.com>
2020-05-26 14:37:35 +0900

    remindのgetの条件の中にsql操作を移動

--- app.py
+++ app.py
@@ -82,40 +82,40 @@ def remind():
             # theme_id が設定されていないとトップページに戻す
             return render_template("index.html")
         else:
-            return render_template("remind.html")
+            #sessionからtheme_idを取得
+            theme_id = session['theme_id']
+            remind = request.form.get('remind')
+
+            # テーマ名を取ってくる
+            conn = sqlite3.connect("servise.db")
+            c = conn.cursor()
+            c.execute("select theme from themes where id = ?", (theme_id,))
+            theme = c.fetchone()
+            c.close()
+
+            # しりとりの内容を取ってくる
+            conn = sqlite3.connect("servise.db")
+            c = conn.cursor()
+            c.execute("select word,id from shiritori where theme_id = ?", (theme_id,))
+            shiritori_list = []
+            # shiritori_listに取ってきた内容を辞書型格納
+            for row in c.fetchall():
+                shiritori_list.append({'id': row[0], 'word': row[1]})
+            c.close()  # closeはfor文の外で！
+            # 中身を確認
+            print(shiritori_list)
+            shiritori_id = shiritori_list[0]
+
+            #db remind に 連想をインサート
+            conn = sqlite3.connect('service.db')
+            c = conn.cursor()
+            c.execute("insert into remind values(?,?,?)", (theme_id,shiritori_id,remind,) )
+            conn.commit()
+            conn.close()
+
+            return render_template("remind.html", shiritori_word = shiritori_list[1], theme = theme)
     else:
-        #sessionからtheme_idを取得
-        theme_id = session['theme_id']
-        remind = request.form.get('remind')
-
-        # テーマ名を取ってくる
-        conn = sqlite3.connect("servise.db")
-        c = conn.cursor()
-        c.execute("select theme from themes where id = ?", (theme_id,))
-        theme = c.fetchone()
-        c.close()
-
-        # しりとりの内容を取ってくる
-        conn = sqlite3.connect("servise.db")
-        c = conn.cursor()
-        c.execute("select word,id from shiritori where theme_id = ?", (theme_id,))
-        shiritori_list = []
-        # shiritori_listに取ってきた内容を辞書型格納
-        for row in c.fetchall():
-            shiritori_list.append({'id': row[0], 'word': row[1]})
-        c.close()  # closeはfor文の外で！
-        # 中身を確認
-        print(shiritori_list)
-        shiritori_id = shiritori_list[0]
-
-        #db remind に 連想をインサート
-        conn = sqlite3.connect('service.db')
-        c = conn.cursor()
-        c.execute("insert into remind values(?,?,?)", (theme_id,shiritori_id,remind,) )
-        conn.commit()
-        conn.close()
-
-        return render_template("remind.html", shiritori_word = shiritori_list[1], theme = theme)
+        return redirect("favorite.html")
 
 
 
