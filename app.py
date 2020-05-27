4d123bcb97e5e324d23fdb8bb470daea8e5f5836
Mio Orita <sakuraminoru0414@gmail.com>
2020-05-27 14:57:09 +0900

    remind,favorite

--- app.py
+++ app.py
@@ -107,21 +107,34 @@ def remind():
 
             # wordだけのリスト型
             s_word = [w['word'] for w in shiritori_list] # ['ひ','ふ','み']
-            for k in s_word:
-                print(k) # ひ ふ み
+            # for k in s_word:
+            #     print(k) # ひ ふ み
 
-            # in range (len(shiritori_list))
+            # for s_len in shiritori_list in range (len(shiritori_list))
 
-            return render_template("remind.html", shiritori_word = k , theme = theme_tuple[0])
+
+            return render_template("remind.html", shiritori_word = s_word , theme = theme_tuple[0])
     else:
+        theme_id = session['theme_id']
+        # しりとりのidを取ってくる
+        conn = sqlite3.connect("service.db")
+        c = conn.cursor()
+        c.execute("select id,word from shiritori where theme_id = ?", (theme_id,))
+        shiritori_list = []
+        # shiritori_listに取ってきた内容をリスト内辞書型格納
+        for row in c.fetchall():
+            shiritori_list.append({'id': row[0], 'word': row[1]})
+        c.close()
         # しりとりidリスト型
         shiritori_id = [d['id'] for d in shiritori_list] # [1,2,3]
         for k in shiritori_id:
             print(k) # 1 2 3
+        r = request.form.getlist("remind")
         #db remind に 連想をインサート
         conn = sqlite3.connect('service.db')
         c = conn.cursor()
-        c.execute("insert into remind values(?,?,?,null)", (theme_id,shiritori_id,remind,))
+        for i in r:
+            c.execute("insert into remind values(?,null,?,null,0)", (theme_id,i,))
         conn.commit()
         conn.close()
         return render_template("favorite.html")
@@ -134,19 +147,26 @@ def favorite():
             return render_template("index.html")
         else:
             theme_id = session['theme_id']
-            conn = sqlite3.connect("servise.db")
+            conn = sqlite3.connect("service.db")
             c = conn.cursor()
-            c.execute("select remind from remind where theme_id = ?", (theme_id,))
+            c.execute("select theme from themes where id = ?", (theme_id,))
+            theme = c.fetchone()
+            c.execute("select remind,id from remind where theme_id = ?", (theme_id,))
             remind_list = []
             for row in c.fetchall():
-                remind_list.append({'remind': row[0]})
+                remind_list.append({'remind': row[0],'id':row[1]})
             c.close() 
-            return render_template("favorite.html",remind_list = remind_list)
+            return render_template("favorite.html",remind_list = remind_list,theme = theme)
     else:
-        conn = sqlite3.connect("servise.db")
+        favs = request.form.getlist("fav")
+        conn = sqlite3.connect("service.db")
         c = conn.cursor()
-        c.execute("select remind from remind where theme_id = ?", (theme_id,))
+        for i in favs:
+            c.execute("update remind set favorite = 1 where id = ?", (i,))
+        conn.commit()
+        c.close()
         return redirect('/finish')
+
 
 @app.route('/finish',methods=["GET","POST"])
 def finish():
@@ -155,9 +175,20 @@ def finish():
             # theme_id が設定されていないとトップページに戻す
             return render_template("index.html")
         else:
-            return render_template("favorite.html")
+            theme_id = session['theme_id']
+            conn = sqlite3.connect("service.db")
+            c = conn.cursor()
+            c.execute("select theme from themes where id = ?", (theme_id,))
+            theme = c.fetchone()
+            c.execute("select remind from remind where favorite = 1")
+            favorite_list = []
+            for row in c.fetchall():
+                favorite_list.append({'remind': row[0]})
+            c.close()
+            return render_template("finish.html",favorite_list = favorite_list, theme = theme)
     else:
         return redirect('/')
+
 
 
 ## おまじない
